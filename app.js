cdbc0d424e212f6743a73a8e05a64b460187f889
sai6993 <155811545+sai6993@users.noreply.github.com>
2025-06-10 00:48:24 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.end("Hello from Node App!");
+});
+server.listen(port, () => {
+  console.log("Server running on port", port);
+});
